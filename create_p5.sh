3e83fca44a8be94f112c7b25772a3527bffd01b2
Cristian727 <cristian.gonzalez7@hotmail.com>
2024-10-28 09:35:41 +0100

    build

--- create_p5.sh
+++ create_p5.sh
@@ -4,3 +4,21 @@ mkdir p5
 touch index.html
 touch sketch.js
 
+echo "<h1>Hola mundo</h1>" > p5/index.html
+
+echo `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.10.0/p5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.10.0/addons/p5.sound.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8" />
+
+  </head>
+  <body>
+    <main>
+    </main>
+    <script src="sketch.js"></script>
+  </body>
+</html>
+`
